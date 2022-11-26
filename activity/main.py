085eae5e5d672c133135502fbab8129785eacaf5
Lisa Utsett <lisabethutsett@gmail.com>
2022-11-25 21:09:28 -0500

    Added tests for handling exceptions ZeroByDivisionError, KeyError, and IndexError. Added functions in main to throw said exceptions.

---
+++ activity/main.py
@@ -0,0 +1,20 @@
+def div_zero(num):
+    x = num / 0
+
+    # KeyError
+def get_value(key):
+    my_dict = {1:"Hello", 2:"World!"}
+    result = my_dict[key]
+
+# IndexError
+def get_element(index):
+    my_list = [2,4,6,7]
+    element = my_list[index]
+
+ # AttributeError
+    # TypeError
+    # NameError
+    # ValueError
+    # UnboundLocalError
+    # OverflowError
+
--- tests/test_exception.py
+++ tests/test_exception.py
@@ -3,4 +3,25 @@ import pytest
 from activity.main import *
 
 def test_exception():
-    assert True
+    num = 5
+    with pytest.raises(ZeroDivisionError):
+        div_zero(num)
+
+ # KeyError
+def test_get_value_key_error():
+    key = 4
+    with pytest.raises(KeyError):
+        get_value(key)
+
+  # IndexError
+def test_get_element_index_error():
+    index = 10
+    with pytest.raises(IndexError):
+        get_element(index)
+  
+    # AttributeError
+    # TypeError
+    # NameError
+    # ValueError
+    # UnboundLocalError
+    # OverflowError
