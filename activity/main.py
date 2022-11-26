d58a0a2014bbe1624044d235b5052e977d5068c7
Lisa Utsett <lisabethutsett@gmail.com>
2022-11-25 22:31:15 -0500

    Added tests for handling exceptions TypeError, AttributeError, NameError, ValueError, UnboundLocalError, and OverflowError.  Added functions in main to throw said exceptions.

--- activity/main.py
+++ activity/main.py
@@ -11,10 +11,30 @@ def get_element(index):
     my_list = [2,4,6,7]
     element = my_list[index]
 
- # AttributeError
-    # TypeError
+# TypeError
+def calculate_sum(b):
+    a=10
+    sum = a+b
+
+    # AttributeError
+def append_to_num(num):
+    num.append(7)
+
     # NameError
+def add_names(first_name):
+    full_name = first_name + last_name
+    
     # ValueError
+def convert_to_int(old_num):
+    new_num = int(old_num)
     # UnboundLocalError
+def print_full_name(last_name):
+    print(first_name + '' + last_name)
+    first_name = "Lisa"
+
     # OverflowError
+def multiply_num(big_num):
+    for i in range(1, 100):
+        big_num = big_num ** i
+
 
--- tests/test_exception.py
+++ tests/test_exception.py
@@ -19,9 +19,38 @@ def test_get_element_index_error():
     with pytest.raises(IndexError):
         get_element(index)
   
-    # AttributeError
     # TypeError
+def test_calculate_sum_type_error():
+    b="Hello World!"
+    with pytest.raises(TypeError):
+        calculate_sum(b)
+
+    # AttributeError
+def test_append_to_num_attribute_error():
+    num = 5
+    with pytest.raises(AttributeError):
+        append_to_num(num)
+
     # NameError
+def test_add_names_name_error():
+    first_name = "Lisa"
+    with pytest.raises(NameError):
+        add_names(first_name)
+
     # ValueError
+def test_convert_to_int_value_error():
+    num = "whoops"
+    with pytest.raises(ValueError):
+        convert_to_int(num)
+
     # UnboundLocalError
+def test_print_full_name_unbound_local_error():
+    last_name = "Utsett"
+    with pytest.raises(UnboundLocalError):
+        print_full_name(last_name)
+
     # OverflowError
+def test_multiply_num_overflow_error():
+    big_num = 7.0
+    with pytest.raises(OverflowError):
+        multiply_num(big_num)
